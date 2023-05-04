a6bef14c699fce2a103275025ad57979dfca40a6
Moein <pistolparody@gmail.com>
2023-05-04 09:41:05 +0330

    Achieved basic functionality for core.grid_system

--- core/game.py
+++ core/game.py
@@ -5,8 +5,9 @@ from core.grid_system import GridSystem
 
 class Game:
     def __init__( self ):
-        self.rect = cr.screen.get_rect()
+        self.rect = FRect(cr.screen.get_rect())
         self.rect.y += self.rect.h * 0.1
+        self.grid_system = GridSystem(self.rect,Vector2(12*5,10*5))
     
     def check_events( self ):
         ...
@@ -14,3 +15,5 @@ class Game:
     def render( self ):
         cr.screen.fill("gray")
         pg.draw.rect(cr.screen,Color("gray").lerp("black",0.5),self.rect)
+
+        self.grid_system.render()
--- core/grid_system.py
+++ core/grid_system.py
@@ -2,5 +2,19 @@ from core.common.names import *
 import core.common.resources as cr
 
 class GridSystem:
-    def __init__(self):
-        ...
+    def __init__(self,rect:FRect,grid_size:Vector2):
+        self.rect = rect
+        self.grid_size = grid_size
+        self.x_step = self.rect.w / grid_size.x
+        self.y_step = self.rect.h / grid_size.y
+
+    def render( self ):
+        for x in range(int(self.grid_size.x)):
+            x *= self.x_step
+            x += self.rect.x
+            pg.draw.line(cr.screen,"black",(x,self.rect.y),(x,self.rect.y+self.rect.h))
+
+        for y in range(int(self.grid_size.y)):
+            y *= self.y_step
+            y += self.rect.y
+            pg.draw.line(cr.screen,"black",(self.rect.x,y),(self.rect.x+self.rect.w,y))
