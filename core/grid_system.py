a6247048b390d1db162a7d28b71509f9b2bb0e2b
Moein <pistolparody@gmail.com>
2023-05-04 09:48:11 +0330

    Added cell rendering system to core.grid_system

--- core/grid_system.py
+++ core/grid_system.py
@@ -7,6 +7,20 @@ class GridSystem:
         self.grid_size = grid_size
         self.x_step = self.rect.w / grid_size.x
         self.y_step = self.rect.h / grid_size.y
+        self.cell_list: list[list[tuple[Vector2,Color]]] = []
+
+
+    def render_cells( self ):
+        for list_ in self.cell_list:
+            for pos,color in list_:
+                x,y = int(pos.x),int(pos.y)
+
+                x *= self.x_step
+                x += self.rect.x
+                y *= self.y_step
+                y += self.rect.y
+
+                pg.draw.rect(cr.screen,color,FRect(x,y,self.x_step,self.y_step))
 
     def render( self ):
         for x in range(int(self.grid_size.x)):
@@ -18,3 +32,5 @@ class GridSystem:
             y *= self.y_step
             y += self.rect.y
             pg.draw.line(cr.screen,"black",(self.rect.x,y),(self.rect.x+self.rect.w,y))
+
+        self.render_cells()
